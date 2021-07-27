87d1e110f7c9232befbd9766dcae26467efa539b
Dmitry Ledentsov <dmlled@yahoo.com>
2021-07-27 21:43:47 +0200

    one more native test

---
+++ src/native-test/java/com/github/d/led/MathsTestIT.java
@@ -0,0 +1,8 @@
+package com.github.d.led;
+
+import com.github.d.led.services.MathsTest;
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class MathsTestIT extends MathsTest {
+}
--- src/test/java/com/github/d/led/services/MathsTest.java
+++ src/test/java/com/github/d/led/services/MathsTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class MathsTest {
+public class MathsTest {
 
     Maths maths = new Maths();
 
